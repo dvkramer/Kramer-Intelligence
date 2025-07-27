4087fe83b86ea4b283617537117f298685482ed9
Derpy153 <derpy153@gmail.com>
2025-07-27 17:42:08 -0400

    new

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js') // We'll create this file, but it can be empty for now
+    }
+  });
+
+  // This line will load your web app's HTML file
+  mainWindow.loadFile('public/index.html');
+  mainWindow.maximize();
+  mainWindow.setMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
